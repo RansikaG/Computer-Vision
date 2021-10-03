8806b31cadc0ee263c547615e21e2616f8345072
RansikaG <ransika1998@gmail.com>
2021-10-03 21:29:38 +0530

    fps

--- PoseEstimationProject/PoseEstimationMin.py
+++ PoseEstimationProject/PoseEstimationMin.py
@@ -1,10 +1,18 @@
 import cv2
 import mediapipe as mp
+import os
+import time
+os.getcwd()
 
 cap = cv2.VideoCapture('PoseVideos/1.mp4')
-
+pTime=0
 while True:
     success, img =cap.read()
-    cv2.impshow("Image", img)
 
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)),(70,50),cv2.FONT_HERSHEY_PLAIN, 3 ,(255,255,0),3)
+    cv2.imshow("Image", img)
     cv2.waitKey(1)
